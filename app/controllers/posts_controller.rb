ef787092c1506309a6cba15775ebf72a6ceac914
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-04-11 13:28:13 -0300

    add posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    @post =Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def destroy
+    
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
