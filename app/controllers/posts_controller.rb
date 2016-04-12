9e578173efa9ec7207756fed9959c0b0f791ba3a
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-04-12 14:07:11 -0300

    edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,8 +20,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+    end
   end
 
   private 
